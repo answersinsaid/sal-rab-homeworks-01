2876aa9039fc4a89b0df9b3ead77d87b2083cc8b
«answersinside@mail.ru <answersinsaid@users.noreply.github.com>
2023-05-02 20:31:29 +0300

    задание 2 выполнил

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,7 +7,7 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum;// создайте переменную shippingSum
 
-if (productsSum=0 || productsSum>=freeShippingMinSum) {
+if (productsSum==0 || productsSum>=freeShippingMinSum) {
     shippingSum=0; 
 
 }else{
